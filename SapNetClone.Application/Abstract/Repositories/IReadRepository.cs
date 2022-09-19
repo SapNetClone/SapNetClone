0f4416a664edac5ed7dca767a283e306b096a270
MEHMET ALİ ÖZDEMİR <mehmetali.ozdemir@bahcesehir.edu.tr>
2022-09-19 23:54:57 +0300

    abstract

---
+++ SapNetClone.Application/Abstract/Repositories/IReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using SapNetClone.Entities.Classes.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapNetClone.Application.Abstract.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : BaseObject
+    {
+    }
+}
---
+++ SapNetClone.Application/Abstract/Repositories/IWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using SapNetClone.Entities.Classes.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapNetClone.Application.Abstract.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : BaseObject
+    {
+
+    }
+}
